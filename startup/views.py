0d35c7359bf9d6993af6cb9b41d91d313104f2dd
harinisureshb <harinibr4545@gmail.com>
2023-03-07 00:05:09 +0530

    first commit

---
+++ startup/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def harini(request):
+    return HttpResponse('<marquee><h1> DAD LITTILE PRINCES </h1></marquee> ')
+def suresh(request):
+    return HttpResponse ('<h1>HERO:</h1> ')
